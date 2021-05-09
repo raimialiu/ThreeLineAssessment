045196b392d2a198c404f26c47ea38ca12d23bde
NTNIGE\A241400 <olatunde.raimi@stanbicibtc.com>
2021-05-09 10:56:55 +0100

    added card dto

--- QuestionTwo/Model/CardDTO.cs
+++ QuestionTwo/Model/CardDTO.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QuestionTwo.Service
+namespace QuestionTwo.Model
 {
-    public interface ICardService
+    public class CardDTO
     {
     }
 }
