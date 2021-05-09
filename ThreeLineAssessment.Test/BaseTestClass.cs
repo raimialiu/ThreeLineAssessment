3c447f609e095308a55d40ef94bdbc3ab909872f
NTNIGE\A241400 <olatunde.raimi@stanbicibtc.com>
2021-05-09 15:28:04 +0100

    added unit test

---
+++ ThreeLineAssessment.Test/BaseTestClass.cs
@@ -0,0 +1,16 @@
+﻿using QuestionTwo.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeLineAssessment.Test
+{
+    public class BaseTestClass
+    {
+        protected ICardService _sut;
+        public BaseTestClass()
+        {
+            _sut = new CardService();
+        }
+    }
+}
---
+++ ThreeLineAssessment.Test/CardServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using QuestionTwo.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ThreeLineAssessment.Test
+{
+    public class CardServiceTest : BaseTestClass
+    {
+        [Fact]
+        public async Task ShouldReturnInvalidWhenInvalidDataIsPassed()
+        {
+            //arrange
+            var CardPan = "6745783784578wjraew";
+            var crdTo = new CardDTO()
+            {
+                type = CardType.CREDIT,
+                Scheme = Scheme.AMEX
+            };
+            // act
+            var result = _sut.VerifyCard(CardPan, crdTo);
+            // assert
+            result.success.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ShouldReturnTrueWhenValidDataIsUsed()
+        {
+            //arrange
+            var CardPan = "7578458754895489589";
+            var crdTo = new CardDTO()
+            {
+                type = CardType.CREDIT,
+                Scheme = Scheme.AMEX
+            };
+            // act
+            var result = _sut.VerifyCard(CardPan, crdTo);
+            // assert
+            result.success.Should().BeTrue();
+        }
+    }
+}
